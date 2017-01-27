b3708c6f19b56973c0027b036fb2616b81f440c9
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-01-27 18:04:53 -0500

    Little refactor

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,16 +4,16 @@ class ProductsController < ApplicationController
 
   def search
     @query = @params[:q]
-    if @query
-      if @category.nil?
-        @products = Product.by_name_query(@query).by_price_range(@price_range[0], @price_range[1])
+    if @query && @query != ''
+      if @cat.nil?
+        @prods = Product.by_name(@query).by_price_range(@p_r[0], @p_r[1]).sort_by &:price
       else
-        @products = Product.by_name_query(@query).by_category(@category).by_price_range(@price_range[0], @price_range[1])
+        @prods = Product.by_name(@query).by_category(@cat).by_price_range(@p_r[0], @p_r[1]).sort_by &:price
       end
-      if @products == []
+      if @prods == []
         render(json: { errors: ['no matching products'] }, status: :not_found)
       else
-        render json: @products
+        render json: @prods
       end
     else
       render(json: { errors: ['please introduce search query'] }, status: :not_found)
@@ -27,15 +27,15 @@ class ProductsController < ApplicationController
   end
 
   def set_category
-    @category = Category.find_by(id: @params[:cat].to_i)
-    if @params[:cat] && !@category && (@params[:cat] != '')
+    @cat = Category.find_by(id: @params[:cat].to_i)
+    if @params[:cat] && !@cat && (@params[:cat] != '')
       render(json: { errors: ['category not found'] }, status: :not_found)
     end
   end
 
   def set_price_range
-    @price_range = []
-    (@params[:minprice] == nil || @params[:minprice] == '') ? @price_range[0] = 0 : @price_range[0] = @params[:minprice].to_i
-    (@params[:maxprice] == nil || @params[:maxprice] == '') ? @price_range[1] = Float::INFINITY : @price_range[1] = @params[:maxprice].to_i
+    @p_r = []
+    @params[:minprice].nil? || @params[:minprice] == '' ? @p_r[0] = 0 : @p_r[0] = @params[:minprice].to_i
+    @params[:maxprice].nil? || @params[:maxprice] == '' ? @p_r[1] = Float::INFINITY : @p_r[1] = @params[:maxprice].to_i
   end
 end
