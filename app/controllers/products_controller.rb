00bdd63cdc285cbf36a06ecb35f105f94b2de0f2
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-01-27 15:52:45 -0500

    Add search by category functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,18 +4,28 @@ class ProductsController < ApplicationController
 
   def search
     @query = @params[:q]
-    @products = Product.by_name_query(@query)
-    render json: @products
-    # render json: @products.first, serializer: ProductSerializer
+
+    if @query
+      @products = Product.by_name_query(@query).by_category(@category) if @query && @category
+      @products = Product.by_name_query(@query) if @query && !@category
+      if @products == []
+        render(json: { errors: ['no matching products in the category'] }, status: :not_found)
+      else
+        render json: @products
+      end
+    else
+      render(json: { errors: ['please introduce search query'] }, status: :not_found)
+    end
   end
 
   def set_params
     @params = params.permit(:q, :minprice, :maxprice, :cat)
   end
 
-
   def set_category
     @category = Category.find_by(id: @params[:cat].to_i)
-    render json: { errors: ['category not found' ] }, status: :not_found and return unless @category
+    if @params[:cat] && !@category && (@params[:cat] != '')
+      render(json: { errors: ['category not found'] }, status: :not_found)
+    end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
 	belongs_to :category
   scope :by_name_query, -> (name_query) { where("name like ?", "%#{name_query}%") }
+  scope :by_category, -> (cat) { where(category: cat) }
 end
