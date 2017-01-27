c39685e7c800056107bee6f6dcb88675a373a7ec
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-01-27 16:06:12 -0500

    Change Product.price datatype

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127055221) do
+ActiveRecord::Schema.define(version: 20170127205430) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170127055221) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name",        limit: 255
-    t.string   "price",       limit: 255
+    t.float    "price",       limit: 24
     t.string   "image",       limit: 255
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
